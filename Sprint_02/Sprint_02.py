8f1f5a63de18ca63077cf20e99ba7350d13aafaa
Deep Patel <deeppatel074@gmail.com>
2023-03-02 15:51:19 -0500

    Added US09 and US10

--- Sprint_02/Sprint_02.py
+++ Sprint_02/Sprint_02.py
@@ -8,7 +8,7 @@ individuals_json = []
 families_json = []
 x.field_names = ["ID","Name", "Gender", "Birthday", "Age","Alive","Death","Child","Spouse"]
 
-file = open('gedcom.ged',mode='r') 
+file = open('C:\\Users\Patel\OneDrive\Desktop\GEDCOM_Team4\Sprint_02\gedcom.ged',mode='r') 
 content = file.readlines()
 dict_name = {}
 sex = "N/A"
@@ -185,7 +185,66 @@ print("Family Table ------------------------------------------------------------
 print(y)
 
 
+def US09():
+    error_story9 = []
+    for item in families_json: 
+        item = json.loads(item)
+        fam_child = item["children"]
+        convert_string = eval(fam_child) if fam_child!="N/A" else "N/A"
+        for i in individuals_json:
+            i = json.loads(i)
+            if i["id"] == item["wife_id"]:
+                if i["death_date"] != 'N/A' and convert_string != "N/A":
+                    for j in individuals_json:
+                        j = json.loads(j)
+                        if j["id"] in convert_string:
+                            if j["birth_date"] != "N/A":
+                                if parse(j["birth_date"]) > parse(i["death_date"]):
+                                    error_story9.append("ERROR: Birth date for individual " + j["id"] + " is after the mother death date")
+            elif i["id"] == item["husband_id"]:
+                if i["death_date"] != 'N/A' and convert_string != "N/A":
+                    for j in individuals_json:
+                        j = json.loads(j)
+                        if j["id"] in convert_string:
+                            if j["birth_date"] != "N/A":
+                                x = parse(j["birth_date"]) - parse(i["death_date"])
+                                arr = str(x)
+                                days = int(arr.split(" ")[0])
+                                if days>270:
+                                    error_story9.append("ERROR: Birth date for individual " + j["id"] + " is after 9 months of the father death date")    
+    return error_story9
 
+print(US09())
+
+def US10():
+    error = []
+    for item in families_json: 
+        item = json.loads(item)
+        if item['marriage_date'] != "N/A":
+            hus_id = item["husband_id"]
+            wfs_id = item["wife_id"]
+            marr_date = item["marriage_date"]
+            for i in individuals_json:
+                i = json.loads(i)
+                if i["birth_date"] != "N/A":
+                    if i["id"] == hus_id:
+                        x = parse(marr_date) - parse(i["birth_date"])
+                        arr = str(x)
+                        days = int(arr.split(" ")[0])
+                        if days >= 0:
+                            if days <= 5110:
+                                error.append("ERROR: Marriage for "+ i["id"]+" should be after 14 year of the age")
+
+                    elif i["id"] == wfs_id:
+                        x = parse(marr_date) - parse(i["birth_date"])
+                        arr = str(x)
+                        days = int(arr.split(" ")[0])
+                        if days >= 0:
+                            if days <= 5110:
+                                error.append("ERROR: Marriage for "+ i["id"]+" should be after 14 year of the age")
+    return error
+
+print(US10())
 
     
     
